62936792db59dfd2ad8b6a44573e7ba52f1e774b
Ahmad <mohammadahmad100k@gmail.com>
2022-01-12 23:35:51 +0500

    Update queries and make controller lightweight

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,18 +12,15 @@ class BooksController < ApplicationController
 
   def assign_tag
     tag = Tag.find(params[:tag_id])
-    if tag.present?
-      Book.where(id: params[:books_ids]).each do |b|
-        BookTag.find_or_create_by(book_id: b.id, tag_id: tag.id)
-      end
-    end
+    Book.assign_book_tag(tag, params[:books_ids]) if tag.present?
+
     redirect_to root_path
   end
 
   private
 
   def fetch_books
-    @books = Book.all
+    @books = Book.includes(:tags)
     @books = @books.joins(:tags).where('tags.id = ?', params[:tag_id]) if params[:tag_id].present?
     @books = @books.where(price: params[:price].to_i) if params[:price].present?
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
   has_many :tags, through: :book_tags
 
   validates :title, uniqueness: true, presence: true
+
+  def self.assign_book_tag(tag, books_ids)
+    where(id: books_ids).each do |book|
+      BookTag.find_or_create_by(book_id: book.id, tag_id: tag.id)
+    end
+  end
 end
